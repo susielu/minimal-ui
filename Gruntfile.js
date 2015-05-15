d7aa671dda880351c52e9fab0b050f3b094f1fe7
Susie Lu <susie.lu.shan@gmail.com>
2015-05-15 13:19:47 -0700

    adding base styles and setup grunt

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      dist: {
+        files: {
+          'minimal-ui.css' : 'sass/minimal-ui.scss'
+        }
+      }
+    },
+
+    watch: {
+      css: {
+        files: ['sass/*.scss'],
+        tasks: ['sass']
+      }
+    },
+
+    cssmin : {
+      minify: {
+        files:[{
+          expand: true,
+          cwd: './',
+          src: ['minimal-ui.css'],
+          dest: '.',
+          ext: '.min.css'
+        }]
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  grunt.registerTask('default',['watch', 'cssmin']);
+  grunt.registerTask('min',['cssmin']);
+
+
+};
